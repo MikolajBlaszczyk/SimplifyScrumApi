acf33c26256f0230f97cb9fab06e9feba58fc1fe
Mikolaj <mikolaj.blaszczyk2002@gmail.com>
2024-08-15 11:42:25 +0200

    Delete old unnecessary files

--- backup/RestoreSimplifyDB.sql
+++
@@ -1,6 +0,0 @@
-RESTORE DATABASE [Simplify] FROM DISK = '/var/opt/mssql/backup/Simplify.bak'
-WITH FILE = 1,
-MOVE 'Simplify' TO '/var/opt/mssql/data/Simplify.mdf',
-MOVE 'Simplify_log' TO '/var/opt/mssql/data/Simplify.ldf',
-NOUNLOAD, REPLACE, STATS = 5;
-GO
